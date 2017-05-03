c58d7d15752b37125a2009376053e64b550c38f2
Diego Millan <diego.millan@edunext.co>
2017-05-03 17:36:26 -0500

    setting ecommerce to process payments with payu
    connecting payu responses
    
    fixing pylint warnings
    
    setting payu response urls correctly

--- ecommerce/extensions/payment/urls.py
+++ ecommerce/extensions/payment/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 
-from ecommerce.extensions.payment.views import PaymentFailedView, SDNFailure, cybersource, paypal, stripe
+from ecommerce.extensions.payment.views import PaymentFailedView, SDNFailure, cybersource, paypal, payu, stripe
 
 CYBERSOURCE_APPLE_PAY_URLS = [
     url(r'^authorize/$', cybersource.CybersourceApplePayAuthorizationView.as_view(), name='authorize'),
@@ -17,6 +17,10 @@ PAYPAL_URLS = [
     url(r'^profiles/$', paypal.PaypalProfileAdminView.as_view(), name='profiles'),
 ]
 
+PAYU_URLS = [
+    url(r'^notify/$', payu.PayUPaymentResponseView.as_view(), name='notify'),
+]
+
 SDN_URLS = [
     url(r'^failure/$', SDNFailure.as_view(), name='failure'),
 ]
@@ -29,6 +33,7 @@ urlpatterns = [
     url(r'^cybersource/', include(CYBERSOURCE_URLS, namespace='cybersource')),
     url(r'^error/$', PaymentFailedView.as_view(), name='payment_error'),
     url(r'^paypal/', include(PAYPAL_URLS, namespace='paypal')),
+    url(r'^payu/', include(PAYU_URLS, namespace='payu')),
     url(r'^sdn/', include(SDN_URLS, namespace='sdn')),
     url(r'^stripe/', include(STRIPE_URLS, namespace='stripe')),
 ]
